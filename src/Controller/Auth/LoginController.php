ac53a734b62870eb146b0bb84cb08e7f5210337a
Hugo Benabdelhak <hugo.ben80000@gmail.com>
2022-09-02 11:32:51 +0200

    Create login form & controller

---
+++ src/Controller/Auth/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Auth;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        if($this->getUser()){
+            return ($this->redirect($this->generateUrl('app_index')));
+        }
+
+        return $this->render('auth/login.html.twig', [
+            "error" => $authenticationUtils->getLastAuthenticationError(),
+            "last_username" => $authenticationUtils->getLastUsername()
+        ]);
+    }
+}
