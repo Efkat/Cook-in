3086bfe9afe5fd6886d25672af50e0ce24a6322a
Efkat <hugo.ben80000@gmail.com>
2022-09-13 16:28:02 +0200

    Set URL for searchController

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
-    #[Route('/search', name: 'app_search')]
-    public function index(): Response
+    #[Route('/search/{recipe}', name: 'app_search', methods: 'GET')]
+    public function index(ManagerRegistry $registry, string $recipe): Response
     {
         return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+            'title'=>$recipe
         ]);
     }
 }
