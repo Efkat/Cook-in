c0bcea4d59b8c6d6948a32af3ad81021a01eae73
Efkat <hugo.ben80000@gmail.com>
2022-09-17 10:49:15 +0200

    Implement /recipes

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class RecipesController extends AbstractController
 {
     #[Route('/recipes', name: 'app_recipes')]
-    public function index(): Response
+    public function index(ManagerRegistry $managerRegistry): Response
     {
+        $recipeRepo = $managerRegistry->getRepository(Recipe::class);
+
+        $recipes = $recipeRepo->findAll();
+
         return $this->render('recipes/index.html.twig', [
-            'controller_name' => 'RecipesController',
+            'title' => 'Recettes',
+            'recipes' => $recipes
         ]);
     }
 }
