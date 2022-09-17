1be6c8b6cbb983adda8e08b452398f969fbaff80
Efkat <hugo.ben80000@gmail.com>
2022-09-17 10:42:48 +0200

    Complete SearchController

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -7,14 +7,20 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\RecipeRepository;
 
 class SearchController extends AbstractController
 {
     #[Route('/search/{recipe}', name: 'app_search', methods: 'GET')]
     public function index(ManagerRegistry $registry, string $recipe): Response
     {
+        $recipeRepo = new RecipeRepository($registry);
+
+        $recipes = $recipeRepo->findAllLike($recipe);
+
         return $this->render('search/index.html.twig', [
-            'title'=>$recipe
+            'title'=>$recipe,
+            'recipes' => $recipes
         ]);
     }
 }
