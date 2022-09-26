3519ff4ca6230af37be6b156b3743f4f4c9925e2
Hugo Benabdelhak <hugo.ben80000@gmail.com>
2022-09-26 14:43:35 +0200

    Add comments to recipe view

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Recipe;
+use App\Repository\CommentRepository;
 use App\Repository\RecipeRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,14 +28,18 @@ class RecipesController extends AbstractController
     #[Route('/recipe/{slug}', name: 'app_recipe_view')]
     public function recipeView(ManagerRegistry $managerRegistry, String $slug): Response
     {
-        $recipeRepo = $managerRegistry->getRepository(Recipe::class);
+        $recipeRepo = new RecipeRepository($managerRegistry);
         $recipe = $recipeRepo->findOneBy(['Slug' => $slug]);
+
+        $commentRepo = new CommentRepository($managerRegistry);
+        $comments = $commentRepo->findBy(['Recipe' => $recipe]);
 
         if($recipe){
 
             return $this->render("recipes/viewRecipe.html.twig", [
                 "title" => $recipe->getTitle(),
-                "recipe" => $recipe
+                "recipe" => $recipe,
+                "comments" => $comments
             ]);
         }else{
             return $this->redirectToRoute('app_index');
